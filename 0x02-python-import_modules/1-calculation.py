877b937037d19b918843e06094a72cc22648aaea
Zoe13Git <zoe13.mail@gmail.com>
2023-03-09 09:12:02 +0000

    update task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
-str = "10 {} 5 = {}"
 if __name__ == "__main__":
-    print(str.format("+", add(a, b)))
-    print(str.format("-", sub(a, b)))
-    print(str.format("*", mul(a, b)))
-    print(str.format("/", div(a, b)))
+    a = 10
+    b = 5
+    str = "{0} {2} {1} = {3}"
+
+    print(str.format(a, b, "+", add(a, b)))
+    print(str.format(a, b, "-", sub(a, b)))
+    print(str.format(a, b, "*", mul(a, b)))
+    print(str.format(a, b, "/", div(a, b)))
